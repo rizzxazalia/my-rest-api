a4fc04739f63b2364bce4b95c51b5abf34241ec1
rizzxazalia <goocrazzy60@gmail.com>
2024-08-18 06:32:31 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://goocrazzy60:rizcry001245@rizzxazalia.qxsjl.mongodb.net/?retryWrites=true&w=majority&appName=rizzxazalia" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "goocrazzy60@gmail.com" //email
+global.email_password = "mdjn cyby tyip lclg" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
